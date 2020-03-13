3509887c0971c468ff78da945fa3166018495e7f
Alex Messina <atm1984@gmail.com>
2020-03-12 22:34:13 -0700

    finished storm 1

--- Data_visualization.py
+++ Data_visualization.py
@@ -14,86 +14,89 @@ pd.set_option('display.width', 180)
 pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 13)
 plt.ion()
+#
+#site_list = 'DELDIOS'
+#site_list = 'FELICITA'
+#site_list = 'KITCARSON'
+site_list = ['CLOVERDALE']
+#site_list = 'GREENVALLEY'
+#site_list = 'MOONSONG'
 
-site = 'DELDIOS'
-site = 'FELICITA'
-site = 'KITCARSON'
-site = 'CLOVERDALE'
-site = 'GREENVALLEY'
-#site = 'MOONSONG'
+#site_list = ['DELDIOS', 'FELICITA','KITCARSON','GREENVALLEY','MOONSONG']#'CLOVERDALE'
 
-
-datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/Data Download/Storm 1/'
-filename = 'LakeHodges_'+site+'_log_20200311.csv'
-#%%
-
-df_all = pd.DataFrame.from_csv(datadir+filename,header=5).reset_index()
-df_all.columns=['Param','Date','Time','Result','Quality']
-df_all['Datetime'] = df_all['Date']+' '+df_all['Time']
-
-if site == 'GREENVALLEY':
-    level = df_all[df_all['Param']=='PT North'][['Datetime','Result']]
-    level['Datetime'] = pd.to_datetime(level['Datetime'])
-    level = level.set_index('Datetime')
+for site in site_list:
+    print site
+    datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/Data Download/Storm 1/'
+    filename = 'LakeHodges_'+site+'_log_20200312.csv'
+    #%%
     
-else:
-    level = df_all[df_all['Param']=='PT Level'][['Datetime','Result']]
-    level['Datetime'] = pd.to_datetime(level['Datetime'])
-    level = level.set_index('Datetime')
-
-
-flow = df_all[df_all['Param']=='Flow_cfs'][['Datetime','Result']]
-flow['Datetime'] = pd.to_datetime(flow['Datetime'])
-flow = flow.set_index('Datetime')
-
-aliquots =  df_all[df_all['Param']=='Triggered S'][['Datetime','Result']]
-aliquots['Datetime'] = pd.to_datetime(aliquots['Datetime'])
-aliquots = aliquots.set_index('Datetime')
-aliquots['Flow'] = flow['Result']
-
-alarm_in = df_all[df_all['Param']=='Alarm In'][['Datetime','Result']]
-alarm_in['Datetime'] = pd.to_datetime(alarm_in['Datetime'])
-alarm_in = alarm_in.set_index('Datetime')
-
-alarm_out = df_all[df_all['Param']=='Alarm Out'][['Datetime','Result']]
-alarm_out['Datetime'] = pd.to_datetime(alarm_out['Datetime'])
-alarm_out = alarm_out.set_index('Datetime')
-
-
-#%%
-fig, ax1 = plt.subplots(1,1,figsize=(16,8))
-fig.suptitle(filename,fontsize=14,fontweight='bold')
-
-ax1.plot_date(level.index,level['Result'],ls='-',marker='None',c='r',label='Water Level from PT')
-ax1.set_ylabel('Water Level (inches)',color='r',fontsize=14,fontweight='bold')
-ax1.spines['left'].set_color('r')
-ax1.tick_params(axis='y',colors='r',labelsize=14)
-ax1.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
-ax1.set_ylim(0, level['Result'].max()*1.25)
-# Delineate alarms
-#ax1.axvline(alarm_in.index[0],c='g')
-#ax1.axvline(alarm_out.index[0],c='r')
-
-ax2 = ax1.twinx()
-ax2.plot_date(flow.index,flow['Result'],ls='-',marker='None',c='b',label='Flow from HvF')
-ax2.plot_date(aliquots.index,aliquots['Flow'],ls='None',marker='o',c='k',label='Aliquots')
-for al in aliquots.iterrows():
-    print al
-    al_num = "%.0f"%al[1]['Result']
-    ax2.annotate(al_num,xy=(pd.to_datetime(al[0]),al[1]['Flow']*1.1),ha='center')
-
-ax2.set_ylabel('Flow (cfs)',color='b',fontsize=14,fontweight='bold')
-ax2.spines['right'].set_color('b')
-ax2.tick_params(axis='y',colors='b',labelsize=14)
-
-ax1.set_ylim(0, level['Result'].max()*1.5)
-
-
-ax1.legend(fontsize=14,ncol=1,loc='upper left')
-ax2.legend(fontsize=14,loc='upper right')
-
-plt.tight_layout()
-plt.subplots_adjust(top=0.95)
+    df_all = pd.DataFrame.from_csv(datadir+filename,header=5).reset_index()
+    df_all.columns=['Param','Date','Time','Result','Quality']
+    df_all['Datetime'] = df_all['Date']+' '+df_all['Time']
+    
+    if site == 'GREENVALLEY':
+        level = df_all[df_all['Param']=='PT North'][['Datetime','Result']]
+        level['Datetime'] = pd.to_datetime(level['Datetime'])
+        level = level.set_index('Datetime')
+        
+    else:
+        level = df_all[df_all['Param']=='PT Level'][['Datetime','Result']]
+        level['Datetime'] = pd.to_datetime(level['Datetime'])
+        level = level.set_index('Datetime')
+    
+    
+    flow = df_all[df_all['Param']=='Flow_cfs'][['Datetime','Result']]
+    flow['Datetime'] = pd.to_datetime(flow['Datetime'])
+    flow = flow.set_index('Datetime')
+    
+    aliquots =  df_all[df_all['Param']=='Triggered S'][['Datetime','Result']]
+    aliquots['Datetime'] = pd.to_datetime(aliquots['Datetime'])
+    aliquots = aliquots.set_index('Datetime')
+    aliquots['Flow'] = flow['Result']
+    
+    alarm_in = df_all[df_all['Param']=='Alarm In'][['Datetime','Result']]
+    alarm_in['Datetime'] = pd.to_datetime(alarm_in['Datetime'])
+    alarm_in = alarm_in.set_index('Datetime')
+    
+    alarm_out = df_all[df_all['Param']=='Alarm Out'][['Datetime','Result']]
+    alarm_out['Datetime'] = pd.to_datetime(alarm_out['Datetime'])
+    alarm_out = alarm_out.set_index('Datetime')
+    
+    
+    #%%
+    fig, ax1 = plt.subplots(1,1,figsize=(16,8))
+    fig.suptitle(filename,fontsize=14,fontweight='bold')
+    
+    ax1.plot_date(level.index,level['Result'],ls='-',marker='None',c='r',label='Water Level from PT')
+    ax1.set_ylabel('Water Level (inches)',color='r',fontsize=14,fontweight='bold')
+    ax1.spines['left'].set_color('r')
+    ax1.tick_params(axis='y',colors='r',labelsize=14)
+    ax1.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
+    ax1.set_ylim(0, level['Result'].max()*1.25)
+    # Delineate alarms
+    #ax1.axvline(alarm_in.index[0],c='g')
+    #ax1.axvline(alarm_out.index[0],c='r')
+    
+    ax2 = ax1.twinx()
+    ax2.plot_date(flow.index,flow['Result'],ls='-',marker='None',c='b',label='Flow from HvF')
+    ax2.plot_date(aliquots.index,aliquots['Flow'],ls='None',marker='o',c='k',label='Aliquots')
+    for al in aliquots.iterrows():
+        print al
+        al_num = "%.0f"%al[1]['Result']
+        ax2.annotate(al_num,xy=(pd.to_datetime(al[0]),al[1]['Flow']*1.1),ha='center')
+    
+    ax2.set_ylabel('Flow (cfs)',color='b',fontsize=14,fontweight='bold')
+    ax2.spines['right'].set_color('b')
+    ax2.tick_params(axis='y',colors='b',labelsize=14)
+    
+    ax1.set_ylim(0, level['Result'].max()*1.5)
+    
+    
+    ax1.legend(fontsize=14,ncol=1,loc='upper left')
+    ax2.legend(fontsize=14,loc='upper right')
+    
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.95)
 
 #%%
 
--- Data_visualization_wRatingTable.py
+++ Data_visualization_wRatingTable.py
@@ -15,26 +15,27 @@ pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 13)
 plt.ion()
 
-site = 'DELDIOS'
+#site = 'DELDIOS'
 #site = 'FELICITA'
 #site = 'KITCARSON'
-##site = 'CLOVERDALE'
+site = 'CLOVERDALE'
 #site = 'GREENVALLEY'
 #site = 'MOONSONG'
+#site = 'SDG_CRK'
 
 
-datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/Data Download/Storm 1/'
-filename = 'LakeHodges_'+site+'_log_20200310.csv'
+datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/Data Download/'
+filename = 'LakeHodges_'+site+'_log_20200312.csv'
 
 rating_curves = pd.ExcelFile(datadir+'Current_RatingCurves.xlsx')
-rating_curve = rating_curves.parse(sheetname='1. Del Dios',skiprows=1,header=0)
+rating_curve = rating_curves.parse(sheetname='4. San Dieguito',skiprows=1,header=0)
 rating_curve = rating_curve.round(2)
 rating_curve.index = rating_curve['Stage (in)']
-#
-#STAGETBL = zip(rating_curve['Stage (in)'].values,rating_curve['Q Total (cfs)'].values)
-#    
-#for i in STAGETBL:
-#    print str((float('%.2f'%i[0]), float('%.2f'%i[1])))+','
+
+STAGETBL = zip(rating_curve['Stage (in)'].values,rating_curve['Q Total (cfs)'].values)
+    
+for i in STAGETBL:
+    print str((float('%.2f'%i[0]), float('%.2f'%i[1])))+','
 
 
 def rating_table(stage_in):
