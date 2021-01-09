0214f397eb8ee0c6454dacb461ce0181f9d48da8
Messina <alex.messina@amecfw.com>
2021-01-08 17:28:30 -0800

    revised all rating curves and scripts for Lake Hodges sites

--- print_ratingcurve_forSutron.py
+++ print_ratingcurve_forSutron.py
@@ -15,23 +15,43 @@ pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 13)
 plt.ion()
 
-#site = 'DELDIOS'
-#site = 'FELICITA'
-#site = 'KITCARSON'
-site = 'CLOVERDALE'
-#site = 'GREENVALLEY'
-#site = 'MOONSONG'
-#site = 'SDG_CRK'
+#site = 'Del Dios'
+#site = 'Felicita'
+#site = 'Kit Carson'
+#site = 'Cloverdale'
+#site = 'Green Valley'
+#site = 'Moonsong'
+#site = 'Sycamore'
+#site = 'San Dieguito'
+#site = 'Guejito'
 
+#site = 'El Ku'
+#site = 'Via Rancho'
+#site = 'Tazon'
+site = 'Oceans11'
+#site = 'Lomica'
 
-datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/Data Download/'
+datadir = 'C:/Users/alex.messina/Documents/GitHub/Sutron_scripts/LakeHodges/Sutron_Scripts/Creeks/'
 
-rating_curves = pd.ExcelFile(datadir+'Current_RatingCurves.xlsx')
-rating_curve = rating_curves.parse(sheetname='4. San Dieguito',skiprows=1,header=0)
-rating_curve = rating_curve.round(2)
+rating_curves = pd.ExcelFile(datadir+'Rating Curves 10_29_2020.xlsx')
+rating_curve = rating_curves.parse(sheetname= site ,skiprows=1,header=0)
+rating_curve = rating_curve.round(3)
 rating_curve.index = rating_curve['Stage (in)']
 
-STAGETBL = zip(rating_curve['Stage (in)'].values,rating_curve['Q Total (cfs)'].values)
+if site=='Cloverdale':
+    rating_curve = rating_curve[ rating_curve['Stage (in)']<75]
     
+list_limit = int(len(rating_curve) / 70)
+
+
+if site == 'Green Valley':
+    list_limit = 1
+
+if 'Flow (cfs)' in rating_curve.columns:
+    STAGETBL = zip(rating_curve['Stage (in)'].values[::list_limit],rating_curve['Flow (cfs)'].values[::list_limit])
+if 'Flow (gpm)' in rating_curve.columns:
+    STAGETBL = zip(rating_curve['Stage (in)'].values[::list_limit],rating_curve['Flow (gpm)'].values[::list_limit])    
+
+
 for i in STAGETBL:
-    print str((float('%.2f'%i[0]), float('%.2f'%i[1])))+','
+    print (   str((float('%.2f'%i[0]), float('%.3f'%i[1])))+','   ) 
